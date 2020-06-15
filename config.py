1bcb904b83eaf4c02c5dbc6562ce77324d8b79db
Apache <apache@peslerbe.com>
2020-06-15 18:45:53 +0200

    new folder for db for scurity reasons (SELinux)

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import credentials
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or "SjwqLxBN5C_WJp3h0pDjWF42YrZF5C1R"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///data.sqlite"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///db/data.sqlite"
---
+++ flaskapp.wsgi
@@ -0,0 +1,4 @@
+#!/usr/bin/python3.6
+import sys
+sys.path.insert(0,"/var/www/cropconnect/")
+from app import app as application
