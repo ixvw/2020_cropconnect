67a8569c745f0b44da4a990370c5f69b5d8557e4
Martin Baldinger <martin.baldinger@ekz.ch>
2020-06-21 20:31:21 +0200

    made path for storing/loading farm imgs from configurable

--- app/util/imgPath.py
+++ app/util/imgPath.py
@@ -10,7 +10,7 @@ def imgPath(filename):
     # check whether we are running on the server
     if sys.path[0] == "/var/www/cropconnect/":
         print("running on the server! serving/storing img from/to resources...")
-        return("resources/" + filename)
+        return(app.config["FARMIMGPATH"] + filename)
     else:
         print("running locally, serving/storing img from/to static...")
         return(os.path.join(APP_FOLDER, Path("app/static/farmimgs/" + filename)))
