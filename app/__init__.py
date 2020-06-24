60b71d0c9cf18e51409a2714237f3ffdc87b6013
Maxime Marchionno <maximem@protonmail.com>
2020-06-24 03:06:53 +0200

    Img bug server fix (#2)
    * correct url in verification link
    
    * all details are shown when any farm button is clicked
    
    * multilanguage (language selected based on browser settings)
    
    * warning in README.md about overwritting translations
    
    * improved README.md regarding translations (update without losing previous work)
    
    * after verification -> show result in normal website framework
    
    * fixed styling of verification page
    
    * Translation and testing
    
    Just some testing and some translation
    
    * updated README.md (use virtualenv for pybabel)
    fixed translation issue (just recompiled the translations using the steps described in the README.md as "Update Translations"
    
    * added delete link to farm details
    
    * only showing "help needed" and "location" in farm list
    
    * basic implementation of img upload (not yet working)
    
    * further work on img upload
    
    * storing in static/farmimgs since this might be the only way for local testing
    
    * img upload and displaying works locally - however, I am not sure whether this also works on the server
    
    * testing img
    
    testing img
    
    * added admin view, discovering whether running locally or on server (to make img upload work while testing)
    
    * made path for storing/loading farm imgs from configurable
    
    * Home and About now aligned to the right
    
    * all fields in farmer form (except photo) are now mandatory
    
    * phone number validator
    
    * added login to access admin-page
    
    * form verification (not yet checking wheter phone is valid)
    
    * secure img name (uuid)
    clean handling of local and remote stuff through app.config["LOCAL"] setting
    
    * small fix
    
    * added a report button. If a farm is reported: send farmid to "hello@cropconnect.world"
    
    * changed e-mail to send reported farms to (cropconnext@gmail.com) because sendgrid e-mails to @cropconnect.ch / world addresses are blocked
    
    * report and delete button styling
    
    * removed farmerfrom.validate() -> seems to cause error on server
    
    * adding reboot_python
    
    * reboot_python v2
    
    * reboot_python v3
    
    * reboot_python v4
    
    * test1
    
    * resources/farmimgs/1.png
    
    Co-authored-by: Martin Baldinger <martin.baldinger@ekz.ch>
    Co-authored-by: admin <hello@cropconnect.world>

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,25 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import Config
+from flask_babel import Babel
+from flask_admin import Admin
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
+babel = Babel(app)
+
 from app.database import db
 db.create_all()
 
+login = LoginManager(app)
+
 from app import routes
 bootstrap = Bootstrap(app)
+
+admin = Admin(app, name="cropconnect", template_mode="bootstrap3")
+from app import adminviews
+
+
+
