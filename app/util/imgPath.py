4de562a6d11541687961ff8de4d80954a5bbba56
admin <hello@cropconnect.world>
2020-06-30 20:58:11 +0200

    URL update

--- app/util/imgPath.py
+++ app/util/imgPath.py
@@ -6,7 +6,7 @@ def imgPath(filename):
     # check whether we are running on the server
     if not app.config["LOCAL"] == "True":
         # print("running on the server! serving/storing img from/to resources...")
-        if variable is None:t
+        if filename is not None:
             return(app.config["FARMIMGPATH"] + str(filename))
         else:
             return("")
